abbc9321ac63a62352a8dcb88957921767453979
g4zeru <haru25haru02@gmail.com>
2021-01-09 22:03:18 +0900

    add metatag and header

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from "next";
+import NextHead from "next/head";
+import Header from "~/components/header";
 
 const RegisterPage: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <NextHead>
+        <title>新規のユーザー登録 - short-story.space</title>
+        <meta property="og:site" content="website" />
+        <meta
+          property="og:title"
+          content="新規ユーザー登録 - short-story.space"
+        />
+        <meta
+          property="og:description"
+          content="ユーザー登録をして自分の思い描いた物語を投稿しよう！"
+        />
+      </NextHead>
+      <Header />
+      <main></main>
+    </>
+  );
 };
 
 export default RegisterPage;
