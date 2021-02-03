59fd5244b9f08b5517b621279440dcebc8579805
g4zeru <haru25haru02@gmail.com>
2021-02-03 21:43:42 +0900

    fix ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,57 +2,10 @@ import Header from "~/components/header";
 import NextHead from "next/head";
 import { NextPage, GetStaticProps } from "next";
 import "firebase/firestore";
-import { firebaseApp } from "~/modules/firebase";
-import { storyConverter, userConverter } from "~/modules/entity";
-import { format } from "~/modules/date";
-import { Content as StoryContent } from "~/components/storycomponent";
 import styled from "styled-components";
 
-export type Content = StoryContent & { user: { id: string; name: string } };
-
-interface Props {
-  contents: Content[];
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const query = firebaseApp()
-    .firestore()
-    .collectionGroup("stories")
-    .where("isPublished", "==", true)
-    .where("isActive", "==", true)
-    .orderBy("createTime", "desc")
-    .withConverter(storyConverter)
-    .limit(3);
-  try {
-    const result = await query.get();
-
-    const contents = await Promise.all(
-      result.docs.map(
-        async (snapshot): Promise<Content> => {
-          const data = snapshot.data();
-          const authorRef = snapshot.ref.parent.parent;
-          const userSnapshot = await authorRef
-            .withConverter(userConverter)
-            .get();
-          const userData = userSnapshot.data();
-          return {
-            id: snapshot.id,
-            user: {
-              id: userSnapshot.id,
-              name: userSnapshot.get("name") ?? "",
-            },
-            title: data.title,
-            body: data.body,
-            createTime: format(userData.createTime.toDate(), "YYYY/MM/DD"),
-          };
-        }
-      )
-    );
-    return { props: { contents }, revalidate: 120 };
-  } catch (e) {
-    console.error(e);
-    return { props: { contents: [] }, revalidate: 30 };
-  }
+export const getStaticProps: GetStaticProps<{}> = async () => {
+  return { props: {} };
 };
 
 const Container = styled.div`
@@ -73,7 +26,7 @@ const Title = styled.h1`
   font-size: 40px;
 `;
 
-const Home: NextPage<Props> = (props) => {
+const Home: NextPage = () => {
   return (
     <Container>
       <NextHead>
